440519b256479aed3e7537abe62f5a0917331cbc
samruddhakarande <samruddha.karande@gmail.com>
2024-04-23 12:34:15 -0500

    Job service application with get, post, delete functionality.

---
+++ src/main/java/com/sam/JobApp/job/impl/JobServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sam.JobApp.job.impl;
+
+import com.sam.JobApp.job.Job;
+import com.sam.JobApp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    List<Job> jobList = new ArrayList<>();
+    private long next = 1L;
+    @Override
+    public List<Job> findAllJobs() {
+        return jobList;
+    }
+
+    @Override
+    public Job postNewJob(Job job) {
+        job.setId(next++);
+        jobList.add(job);
+        return job;
+    }
+
+    @Override
+    public Job findJobById(long id) {
+        for(Job job: jobList) {
+            if(id == job.getId()) {
+                return job;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean deleteJobById(long id) {
+        Iterator<Job> iterator = jobList.iterator();
+        while(iterator.hasNext()){
+            Job job = iterator.next();
+            if(id == job.getId()) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+}
